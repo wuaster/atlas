48a854f3be5486b76161b54c38a4d3a39bb27dfd
Michael Roudnitski <mroudnitski@ibm.com>
2020-07-14 18:45:46 -0400

    Add provider

--- lib/ui/screens/chat_screen.dart
+++ lib/ui/screens/chat_screen.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:dash_chat/dash_chat.dart';
+import 'package:provider/provider.dart';
 
 class ChatScreen extends StatelessWidget {
+  final List<ChatMessage> messages = [
+    ChatMessage(
+      text: "Hello World, this message will be from Watson in due time.",
+      user: ChatUser(name: "Watson"),
+      createdAt: DateTime.now(),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,14 +21,7 @@ class ChatScreen extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(20.0)),
           ),
           onSend: null,
-          messages: [
-            ChatMessage(
-              text:
-                  "Hello World, this message will be from Watson in due time.",
-              user: ChatUser(name: "Watson"),
-              createdAt: DateTime.now(),
-            )
-          ],
+          messages: messages,
           user: ChatUser(name: "Me")),
     );
   }
