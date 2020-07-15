219235c9e614cf0e4c9e491e21981b97697c97c5
ben honda <benhonda@ibm.com>
2020-07-15 17:46:42 -0400

    set-up WA webhooks

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,11 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { default: Axios } = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
+const debug = process.env.NODE_ENV === "production";
 const watsonCreds = {
   version: process.env.WATSON_VERSION || "",
   apiKey: process.env.WATSON_API_KEY || "",
@@ -14,8 +16,14 @@ const watsonCreds = {
   id: process.env.WATSON_ASSISTANT_ID || "",
 };
 
+const hereCreds = {
+  apiKey: process.env.HERE_API_KEY || "",
+  geocodingUrl: process.env.HERE_GEOCODE_ENDPOINT || "",
+  routesUrl: process.env.HERE_ROUTES_ENDPOINT || "",
+};
+
 const watson_err_message =
-    "Your message could not be sent at this time. Please try again or contact support.";
+  "Your message could not be sent at this time. Please try again or contact support.";
 
 function handleError(err) {
   if (err.message && err.status) return `${err.status}: ${err.message}`;
@@ -44,6 +52,7 @@ app.get("/", (_req, res) => {
   res.send("Welcome to the app.");
 });
 
+// create a session id to be used for the duration of the session
 app.get("/session", async (_req, res) => {
   var result = "";
   try {
@@ -62,6 +71,7 @@ app.get("/session", async (_req, res) => {
   }
 });
 
+// post a message and recieve a response from watson
 app.post("/message", async (req, res) => {
   const msgObj = req.body;
   if (!msgObj.sessionId) return res.send("Invalid 'sessionId' field.");
@@ -89,6 +99,145 @@ app.post("/message", async (req, res) => {
   } finally {
     return res.send(result);
   }
+});
+
+async function getCoordinatesFromAddress(address = "") {
+  var result = "Sorry, your start address or destination address is invalid.";
+  var errorFlag = false;
+  try {
+    const geocoded = await Axios({
+      method: "get",
+      url: hereCreds.geocodingUrl,
+      params: {
+        q: address,
+        apiKey: hereCreds.apiKey,
+      },
+    });
+    if (!geocoded.data)
+      return {
+        error: true,
+        result: result,
+      };
+    if (!geocoded.data.items)
+      return {
+        error: true,
+        result: result,
+      };
+    if (!geocoded.data.items[0].position)
+      return {
+        error: true,
+        result: result,
+      };
+    result = geocoded.data.items[0].position;
+  } catch (err) {
+    if (debug) {
+      result = handleError(err);
+    }
+    errorFlag = true;
+  } finally {
+    return {
+      error: errorFlag,
+      result: result,
+    };
+  }
+}
+
+async function getRouteDurationAndLength(origin, dest) {
+  // 'origin' and 'dest' are of the format:
+  // 56.400,40.4014
+  var result = "Sorry, your start address or destination address is invalid.";
+  var errorFlag = false;
+  try {
+    const route = await Axios({
+      method: "get",
+      url: hereCreds.routesUrl,
+      params: {
+        transportMode: "car",
+        origin: origin,
+        destination: dest,
+        return: "summary",
+        apiKey: hereCreds.apiKey,
+      },
+    });
+    if (!route.data)
+      return {
+        error: true,
+        result: result,
+      };
+    if (!route.data.routes)
+      return {
+        error: true,
+        result: result,
+      };
+    if (!route.data.routes[0].sections)
+      return {
+        error: true,
+        result: result,
+      };
+    if (!route.data.routes[0].sections[0].summary)
+      return {
+        error: true,
+        result: result,
+      };
+    result = route.data.routes[0].sections[0].summary;
+  } catch (err) {
+    if (debug) {
+      result = handleError(err);
+    }
+    errorFlag = true;
+  } finally {
+    return {
+      error: errorFlag,
+      result: result,
+    };
+  }
+}
+
+async function getEconomyFromDestination(start_address, end_address) {
+  console.log(`Getting economy from ${start_address} to ${end_address}...`);
+
+  const originCoordinatesObj = await getCoordinatesFromAddress(start_address);
+  if (originCoordinatesObj.error) return routeObj.result;
+  const originCoordinates = `${originCoordinatesObj.result.lat},${originCoordinatesObj.result.lng}`;
+  // first translate address to coordinates
+  const destCoordinatesObj = await getCoordinatesFromAddress(end_address);
+  if (destCoordinatesObj.error) return routeObj.result;
+  const destCoordinates = `${destCoordinatesObj.result.lat},${destCoordinatesObj.result.lng}`;
+  // then get the route
+  const routeObj = await getRouteDurationAndLength(
+    originCoordinates,
+    destCoordinates
+  );
+  // check for error
+  if (routeObj.error) return routeObj.result;
+
+  const routeDuration = routeObj.result.duration;
+  const routeLength = routeObj.result.length;
+
+  // finally, calculate economy
+  const gasMilage = 11.0 / 100.0; // liters/100 km
+  const gasPricePerLiter = 0.9; // in dollars
+  const lengthInKm = routeLength / 1000.0;
+  const totalDollars = gasMilage * lengthInKm * gasPricePerLiter;
+  const roundedTotal = Math.round((totalDollars + Number.EPSILON) * 100) / 100;
+  // return `Cost: ${roundedTotal} CAD. Distance traveled: ${Math.round((lengthInKm + Number.EPSILON) * 100) / 100}km`;
+  return { dollars: roundedTotal };
+}
+
+// endpoint called by the watson assistant via webhooks
+app.post("/watson", async (req, res) => {
+  const params = req.body;
+
+  if (params.start_address && params.end_address) {
+    const response = await getEconomyFromDestination(
+      params.start_address,
+      params.end_address
+    );
+
+    return res.send({ message: response });
+  }
+
+  return res.send("nothing");
 });
 
 app.get("*", (_req, res) => {
