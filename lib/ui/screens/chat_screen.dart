521dac4a3dde7254c2628b90dd67c32a46586522
Michael Roudnitski <mroudnitski@ibm.com>
2020-07-14 23:36:27 -0400

    Refactor chat

--- lib/ui/screens/chat_screen.dart
+++ lib/ui/screens/chat_screen.dart
@@ -1,28 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:dash_chat/dash_chat.dart';
 import 'package:provider/provider.dart';
+import 'package:atlas/ui/components/chat.dart';
 
 class ChatScreen extends StatelessWidget {
-  final List<ChatMessage> messages = [
-    ChatMessage(
-      text: "Hello World, this message will be from Watson in due time.",
-      user: ChatUser(name: "Watson"),
-      createdAt: DateTime.now(),
-    )
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: DashChat(
-          showUserAvatar: false,
-          messageContainerDecoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.all(Radius.circular(20.0)),
-          ),
-          onSend: null,
-          messages: messages,
-          user: ChatUser(name: "Me")),
+      child: Chat(),
     );
   }
 }
