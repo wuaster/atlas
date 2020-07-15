254a3b2bdcf909ea8333f95b145cfde103869372
Michael Roudnitski <mroudnitski@ibm.com>
2020-07-15 15:23:54 -0400

    Fix trip_screen UI

--- lib/ui/screens/trip_screen.dart
+++ lib/ui/screens/trip_screen.dart
@@ -23,63 +23,61 @@ class TripScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          iconTheme: IconThemeData(
-            color: Colors.black87,
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        iconTheme: IconThemeData(
+          color: Colors.black87,
+        ),
+        elevation: 0,
+      ),
+      body: Stack(
+        children: <Widget>[
+          Image(
+            image: AssetImage('assets/graphics/map.png'),
           ),
-          elevation: 0,
-        ),
-        body: Stack(
-          children: <Widget>[
-            Image(
-              image: AssetImage('assets/graphics/map.png'),
-            ),
-            SingleChildScrollView(
-              padding: EdgeInsets.symmetric(horizontal: 32, vertical: 128),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(12),
-                    child: Column(
-                      children: <Widget>[
-                        Text("July 10th 2020"),
-                        Text(
-                          "To $name",
-                          style: Theme.of(context).textTheme.headline5,
-                        ),
-                        stat(
-                          "7 kg CO2",
-                          Icons.local_gas_station,
-                          Colors.blue,
-                        ),
-                        stat(
-                          "\$$amount saved",
-                          Icons.money_off,
-                          Colors.green,
-                        ),
-                        stat(
-                          "$distance km travelled",
-                          Icons.local_gas_station,
-                          Colors.amber,
-                        ),
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10),
+          SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 32, vertical: 128),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(12),
+                  child: Column(
+                    children: <Widget>[
+                      Text("July 10th 2020"),
+                      Text(
+                        "To $name",
+                        style: Theme.of(context).textTheme.headline5,
                       ),
+                      stat(
+                        "7 kg CO2",
+                        Icons.local_gas_station,
+                        Colors.blue,
+                      ),
+                      stat(
+                        "\$$amount saved",
+                        Icons.money_off,
+                        Colors.green,
+                      ),
+                      stat(
+                        "$distance km travelled",
+                        Icons.local_gas_station,
+                        Colors.amber,
+                      ),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
                     ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
