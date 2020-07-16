0bd414caebeaa62f981514b326854d616796cd13
aster <aster@ibm.com>
2020-07-15 21:26:13 -0400

    adding localstorage

---
+++ lib/localstorage.dart
@@ -0,0 +1,23 @@
+import 'package:localstorage/localstorage.dart';
+import 'package:atlas/localstorage.dart';
+
+class LocalStorageRepository implements ILocalStorageRepository {
+  final LocalStorage _storage;
+
+  LocalStorageRepository(String storageKey)
+      : _storage = LocalStorage(storageKey);
+
+  @override
+  Future getAll(String key) async {
+    await _storage.ready;
+
+    return _storage.getItem(key);
+  }
+
+  @override
+  Future<void> save(String key, dynamic value) async {
+    await _storage.ready;
+
+    return _storage.setItem(key, value);
+  }
+}
